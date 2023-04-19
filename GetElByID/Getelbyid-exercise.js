fd9af6ed532ca579ed001027b8e27cd74872389c
agosanna <agostino.sanna@mail.polimi.it>
2023-04-19 14:06:32 +0200

    Capybara changes

--- GetElByID/Getelbyid-exercise.js
+++ GetElByID/Getelbyid-exercise.js
@@ -1,27 +1,26 @@
 let slide_corrente = 1; //1 -> 5
 
 function current_slide() {
-  let slides = document.getElementsByClassName('slide');    //le immagini appartengono tutte alla classe slides
-  let obj_prev = document.getElementById('prev');           //dichiariamo le variabili che pui useremo di seguito
+  let slides = document.getElementsByClassName('slide');   
+  let obj_prev = document.getElementById('prev');         
   let obj_next = document.getElementById('next');
 
-  for (i = 0; i < slides.length; i++) {                   //ciclo iterativo che prende tutte le immagini facendole scomparire
+  for (i = 0; i < slides.length; i++) {                   
     slides[i].style.display = "none";
   }
-  obj_prev.style.display = 'block';                      //le frecce restano visibili
+  obj_prev.style.display = 'block';                    
   obj_next.style.display = 'block';
 
-  slides[slide_corrente-1].style.display = "block";     //mostra solo la prima immagine
-
+  slides[slide_corrente-1].style.display = "block";     
 }
 
-current_slide();                                       //richiamo la funzione in modo che si esegua
+current_slide();                                      
 
 function torna_indietro(){
-  slide_corrente = slide_corrente - 1;                      //vado indietro di una slide
+  slide_corrente = slide_corrente - 1;                     
   let slides = document.getElementsByClassName('slide');
-  if (slide_corrente < 1) slide_corrente = slides.length;  //condizione in cui se torno piu indietro della prima slide riparto dall'ultima
-  current_slide();                                         //e infine richiamo la funzione current slide
+  if (slide_corrente < 1) slide_corrente = slides.length;  
+  current_slide();                                         
 }
 
 function vai_avanti(){
