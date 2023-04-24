7ca246931d4d98996fe129feeb510e5c4d38a85e
agosanna <agostino.sanna@mail.polimi.it>
2023-04-24 16:21:40 +0200

    edit

--- Ex during class/stoplight/sketch.js
+++ Ex during class/stoplight/sketch.js
@@ -1,9 +1,4 @@
-let car;
-
 function setup() {
-    car = {
-        g: 250
-    };
 
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
@@ -19,19 +14,24 @@ function windowResized() {
   function draw() {
     background(	173, 216, 230);
 
-    //ground
+    //traffic light box
     fill(50)
-    rect(0, windowHeight - 30, windowWidth, 50)
+    rectMode(CENTER);    
+    rect(2*windowWidth/3, windowHeight - 180, 10, 320);
+    rect(2*windowWidth/3, windowHeight/2 - 50, 60, 150);
+    rectMode(CORNER);
 
 
     //the car
-    fill(0, car.g, 0);
+    fill(0, 250, 0);
 	rect(mouseX, windowHeight - 100, 110, 50, 20);
 	fill(50);
 	ellipse(mouseX, windowHeight - 50, 40, 40);
 	ellipse(mouseX + 110, windowHeight - 50, 40, 40);
 
-    //traffic light box
+    //ground
+    fill(50);
+    rect(0, windowHeight - 30, windowWidth, 50);
 
     //traffic light
     if(mouseX < windowWidth/3){
@@ -39,22 +39,22 @@ function windowResized() {
     } else {
         fill(50);
     }
-    circle(windowWidth/2 + 200, windowHeight/2 - 100, 30);
+    circle(2*windowWidth/3, windowHeight/2 - 100, 30);
 
-    if(mouseX < 2*windowWidth/3 && mouseX > windowWidth/3){
+    if(mouseX < 2*windowWidth/3 - 100 && mouseX > windowWidth/3){
         fill(255,255,0);
     } else {
         fill(50);
     }
-    circle(windowWidth/2 + 200, windowHeight/2 - 50, 30);
+    circle(2*windowWidth/3, windowHeight/2 - 50, 30);
 
-    if(mouseX > 2*windowWidth/3){
+    if(mouseX > 2*windowWidth/3 - 100){
         fill(0,255,0);
     } else {
         fill(50);
     }
     
-    circle(windowWidth/2 + 200, windowHeight/2, 30);
+    circle(2*windowWidth/3, windowHeight/2, 30);
 
     
 
