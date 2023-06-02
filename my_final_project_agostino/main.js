a5b791705796a32d811c5a0ca1d68a57a41be8d4
agosanna <agostino.sanna@mail.polimi.it>
2023-06-02 13:43:09 +0200

    basic features

--- my_final_project_agostino/main.js
+++ my_final_project_agostino/main.js
@@ -1,13 +1,36 @@
 let container = document.getElementById("container");
 let card = document.getElementsByClassName("card");
+let flippedCards = [];
 
-let n = 40;
+let n = 20;
 
 
-for(let i = 0; i < n; i++){
-    container.innerHTML += `<div class="card" onclick="turnCard(this)">?</div>`
+for(let i = 1; i <= n; i++){
+    container.innerHTML += 
+    `<div class="card" onclick="flipCard(this.querySelector('.front'))">
+        <img class="front" id="img_${i}" src="./assets/${i}.jpg">
+        <img class="back" src= "./assets/back.png">
+    </div>
+    <div class="card" onclick="flipCard(this.querySelector('.front'))">
+        <img class="front" id="img_${i}" src="./assets/${i}.jpg">
+        <img class="back" src= "./assets/back.png">
+    </div>`
 }
 
-const turnCard = (element) => {
-    element.style.backgroundColor = 'blue'
+const flipCard = function(element){
+    element.style.zIndex = "1";
+    flippedCards.push(element.id)
+    if (flippedCards.length === 2){
+        compareCards();
+    }
 }
+
+    const compareCards = () => {
+        if (flippedCards[0] === flippedCards[1]) {
+            console.log('MATCH');
+        } else {
+            console.log('NO MATCH');
+
+        }
+        flippedCards = [];
+    };
